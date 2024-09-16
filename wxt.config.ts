5674d1d1a0214b96858a4cb6464bffdd3345154c
Rafal Iskrzycki <rafal.iskrzycki@allegro.com>
2024-09-16 14:30:32 +0200

    Icon changed. assets cleanup

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   manifest: {
     permissions: ["storage"],
-    version: "1.0",
+    version: "0.1",
     host_permissions: ["https://api.mikr.us/*"],
     action: {
       default_popup: "popup.html",
     },
     name: "mikr.us addon",
-    description: "A Chrome extension to make a POST request with an API key.",
+    description: "An extension that helps you manage your mikr.us server",
   },
 });
